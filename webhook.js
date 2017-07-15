3b66d687ad7129687cded8d34fb11ed651a26c8f
David <mydavidkoh@gmail.com>
2017-07-15 10:59:58 -0700

    first commit

---
+++ webhook.js
@@ -0,0 +1,103 @@
+// http://www.girliemac.com/blog/2017/01/06/facebook-apiai-bot-nodejs/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const apiaiApp = require('apiai')("bd60b525ec224269adc52486f3ee3bc1");
+const request = require('request');
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+/* For Facebook Validation */
+app.get('/webhook', (req, res) => {
+	if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		res.status(403).end();
+	}
+});
+
+/* Handling all messenges */
+app.post('/webhook', (req, res) => {
+	console.log(req.body);
+	if (req.body.object === 'page') {
+		req.body.entry.forEach((entry) => {
+			entry.messaging.forEach((event) => {
+				if (event.message && event.message.text) {
+					sendMessage(event);
+				}
+			});
+		});
+		res.status(200).end();
+	}
+});
+
+app.post('/ai', (req, res) => {
+	if (req.body.result.action === 'weather') {
+		let city = req.body.result.parameters['geo-city'];
+		let WEATHER_API_KEY = "6470a3f87e8e2dcd9e419cb8bcb5b927";
+		let restUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID='+WEATHER_API_KEY+'&q='+city;
+
+		request.get(restUrl, (err, response, body) => {
+			if (!err && response.statusCode === 200) {
+				let json = JSON.parse(body);
+				let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+
+				return res.json({
+					speech: msg,
+					displayText: msg,
+					source: 'weather'});
+			} else {
+				return res.status(400).json({
+					status: {
+						code: 400,
+						errorType: "I failed to look up the city name."}});
+			}})
+	}
+});
+
+
+function sendMessage(event) {
+	let sender = event.sender.id;
+	let text = event.message.text;
+
+	let apiai = apiaiApp.textRequest(text, {
+		sessionId: 'tabby_cat'
+	});
+
+	apiai.on('response', (response) => {
+		let aiText = response.result.fulfillment.speech;
+
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token: "EAABb2UnINZA8BAFaimiuulKaq57REQQlLEC7OCXtThS44i4drW4zcczEDQUZBz1NisbDensCf16ZBpdHXGa7nBCKKMf8PMs527WpNhVtXnASGSUhUDwMMnfXzjZBs6W4JvlyvMpLtNFCR1QNZBFVv3muUvFNqLT4ORzfxj6AyeAZDZD"},
+			method: 'POST',
+			json: {
+				recipient: {id: sender},
+				message: {text: aiText}
+			}
+		}, function (error, response) {
+			if (error) {
+				console.log('Error sending message: ', error);
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error);
+			}
+		});
+
+	});
+
+	apiai.on('error', (error) => {
+		console.log(error);
+	});
+
+	apiai.end();
+
+
+}
+
+
+
